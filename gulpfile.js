1ad21da598fd0b7a77a1427ccb2f38ef926fbd55
tomo <tomo@MacBook-Pro-tomo.local>
2018-05-22 16:01:12 +0200

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,70 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var plumber = require('gulp-plumber');
 var connect = require('gulp-connect');
-var notify = require('gulp-notify');
 var cache = require('gulp-cache');
 var pug = require('gulp-pug');
 var livereload = require('gulp-livereload');
 var del = require('del');
 
+//html
+gulp.task('html', function() {
+    return gulp.src(['**/*.pug', '!node_modules/**'])
+      .pipe(plumber())
+      .pipe(pug({pretty:false, doctype:'HTML'}))
+      .pipe(gulp.dest('_public'))
+});
+
+//styles
+gulp.task('css', function() {
+  return gulp.src(['sass/**/*.*', '!sass/node_modules/**/*.*', '!sass/package-lock.json'])
+    .pipe(plumber())
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version'))
+    .pipe(gulp.dest('_public/css'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(minifycss())
+    .pipe(gulp.dest('_public/css'))
+});
+
+//js
+gulp.task('js', function() {
+  return gulp.src(['js/node_modules/jquery/dist/jquery.js', 'js/*.js'])
+    .pipe(plumber())
+    .pipe(concat('script.js'))
+    .pipe(gulp.dest('_public/js'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(uglify({ mangle: false }))
+    .pipe(gulp.dest('_public/js'))
+});
+
+//images
+gulp.task('img', function() {
+  return gulp.src('img/*')
+    .pipe(plumber())
+    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('_public/img'))
+});
+
+//clean _public
+gulp.task('clean', function(cb) {
+  del(['_public/css/*', '_public/js/*', '_public/img/*'], cb)
+});
+
+//live reload / watch
+gulp.task('watch', function() {
+  gulp.watch('sass/**/*.*', ['css']);
+  gulp.watch(['**/*.pug', 'html/**/*.pug'], ['html']);
+  gulp.watch('js/**/*.js', ['js']);
+  gulp.watch('img/**/*.*', ['img']);
+  gulp.watch('_public/**/*', ['livereload']);
+  
+});
+
+//live reload
+gulp.task('livereload', function() {
+  gulp.src('_public/**/*')
+    .pipe(connect.reload());
+});
 
 //server
 gulp.task('server', function() {
@@ -25,69 +83,10 @@ gulp.task('server', function() {
   });
 });
 
-//html
-gulp.task('html', function() {
-    return gulp.src(['**/*.pug', '!node_modules/**'])
-      .pipe(plumber())
-      .pipe(pug({pretty:false, doctype:'HTML'}))
-      .pipe(gulp.dest('_public'))
-      .pipe(notify({ message: 'HTML done.' }));
-});
-
-//styles
-gulp.task('css', function() {
-  return gulp.src(['sass/**/*.*', '!sass/bulma/**/*.*'])
-    .pipe(plumber())
-    .pipe(sass({ style: 'expanded' }))
-    .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('_public/css'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('_public/css'))
-    .pipe(notify({ message: 'SASS done.' }));
-});
-
-//js
-gulp.task('js', function() {
-  return gulp.src('js/*.js')
-    .pipe(plumber())
-    .pipe(concat('script.js'))
-    .pipe(gulp.dest('_public/js'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest('_public/js'))
-    .pipe(notify({ message: 'SCRIPTS done.' }));
-});
-
-//images
-gulp.task('img', function() {
-  return gulp.src('img/*')
-    .pipe(plumber())
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('_public/img'))
-    .pipe(notify({ message: 'IMG done.' }));
-});
-
-//clean _public
-gulp.task('clean', function(cb) {
-  del(['_public/css/*', '_public/js/*', '_public/img/*'], cb)
-});
-
 //def
-gulp.task('default', ['server', 'clean', 'html', 'css', 'js', 'img'], function() {
-  gulp.start('css', 'js', 'img');
+gulp.task('default', ['server', 'watch', 'html', 'css', 'js', 'img'], function() {
+  gulp.start('css', 'js', 'img', 'html');
 });
 
 
-//live reload / watch
-gulp.task('watch', function() {
-  gulp.watch('*/*.pug', ['html']);
-  gulp.watch('sass/*.*', ['css']);
-  gulp.watch('js/*.js', ['js']);
-  gulp.watch('img/**/*', ['img']);
 
-  livereload.listen();
-
-  gulp.watch(['_public/**']).on('change', livereload.changed);
-
-});
