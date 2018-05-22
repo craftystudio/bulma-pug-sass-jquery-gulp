02d5248dde9c694f7bd5344e3261dab0424e9a3e
tomo <tomo@MacBook-Pro-tomo.local>
2018-05-22 13:56:18 +0200

    gulpfile mods

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
 //setup
-var gulp = require('gulp'),
-  sass = require('gulp-ruby-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  minifycss = require('gulp-minify-css'),
-  uglify = require('gulp-uglify'),
-  imagemin = require('gulp-imagemin'),
-  rename = require('gulp-rename'),
-  concat = require('gulp-concat'),
-  notify = require('gulp-notify'),
-  cache = require('gulp-cache'),
-  livereload = require('gulp-livereload'),
-  del = require('del');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var cache = require('gulp-cache');
+var pug = require('gulp-pug');
+var livereload = require('gulp-livereload');
+var del = require('del');
+
+//html
+gulp.task('html', function() {
+    return gulp.src(['**/*.pug', '!node_modules/**'])
+      .pipe(plumber())
+      .pipe(pug({pretty:false, doctype:'HTML'}))
+      .pipe(gulp.dest('_public'))
+      .pipe(notify({ message: 'HTML done.' }));
+});
 
 //styles
 gulp.task('css', function() {
   return gulp.src('sass/**/*.*')
+    .pipe(plumber())
     .pipe(sass({ style: 'expanded' }))
     .pipe(autoprefixer('last 2 version'))
     .pipe(gulp.dest('_public/css'))
@@ -27,6 +39,7 @@ gulp.task('css', function() {
 //js
 gulp.task('js', function() {
   return gulp.src('js/*.js')
+    .pipe(plumber())
     .pipe(concat('script.js'))
     .pipe(gulp.dest('_public/js'))
     .pipe(rename({ suffix: '.min' }))
@@ -38,6 +51,7 @@ gulp.task('js', function() {
 //images
 gulp.task('img', function() {
   return gulp.src('img/*')
+    .pipe(plumber())
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest('_public/img'))
     .pipe(notify({ message: 'IMG done.' }));
@@ -49,13 +63,15 @@ gulp.task('clean', function(cb) {
 });
 
 //def
-gulp.task('default', ['clean'], function() {
+gulp.task('default', ['clean', 'html', 'css', 'js', 'img'], function() {
   gulp.start('css', 'js', 'img');
 });
 
 
 //live reload / watch
 gulp.task('watch', function() {
+
+  gulp.watch('*/*.pug', ['html']);
 
   gulp.watch('sass/*.*', ['css']);
 
